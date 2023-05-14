a9034ca399c68164ac7e8db30b05d4321157ea12
DaDDy <blacklistuser420@gmail.com>
2023-05-14 19:16:53 +0630

    database set up

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,15 +1,21 @@
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { CartContext } from "../../contexts/cart.context";
 import Button from "../button/button.component";
 import CartItem from "../cart-item/cart-item.component";
 import "./cart-dropdown.styles.scss";
 const CartDropdown = () => {
+  const { cartItems } = useContext(CartContext);
+  const navigate = useNavigate();
+  const goToCheckOut = () => navigate("/checkout");
   return (
     <div className="cart-dropdown-container">
       <div className="cart-items">
-        {[].map((item) => (
-          <CartItem cartItem={item} />
+        {cartItems.map((item) => (
+          <CartItem key={item.id} cartItem={item} />
         ))}
       </div>
-      <Button>Go To Checkout</Button>
+      <Button onClick={goToCheckOut}>Go To Checkout</Button>
     </div>
   );
 };
