e58679b906b9b47bab58aac54ed7d81ef8fa77ab
DaDDy <blacklistuser420@gmail.com>
2023-05-18 12:05:55 +0630

    update

--- src/store/store.js
+++ src/store/store.js
@@ -3,6 +3,12 @@ import { compose, applyMiddleware, createStore } from "redux";
 import logger from "redux-logger";
 import { rootReducer } from "./root-reducer";
 
+const loggerMiddleware = (store) => (next) => (action) => {
+  if (!action.type) {
+    return next(action);
+  }
+};
+
 const middleWares = [logger];
 const composedEnchancers = compose(applyMiddleware(...middleWares));
 export const store = createStore(rootReducer, undefined, composedEnchancers);
