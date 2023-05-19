17a9ee4a463425ee8909bf7bd22c0c7a030b3884
DaDDy <blacklistuser420@gmail.com>
2023-05-19 16:36:54 +0630

    fix categories action error

--- src/store/categories/category.action.js
+++ src/store/categories/category.action.js
@@ -2,32 +2,17 @@ import { CATEGORIES_ACTION_TYPES } from "./category.types";
 import { createAction } from "../../utils/reducer/reducer.utils";
 import { getCategoriesAndDocuments } from "../../utils/firebase.utils";
 
-export const setCategories = (categoriesArray) =>
-  createAction(CATEGORIES_ACTION_TYPES.SET_CATEGORIES, categoriesArray);
-
-export const fetchCategoriesStart = () => {
+export const fetchCategoriesStart = () =>
   createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START);
-};
-export const fetchCategoriesSuccess = (categoriesArray) => {
+
+export const fetchCategoriesSuccess = (categoriesArray) =>
   createAction(
     CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS,
     categoriesArray
   );
-};
-export const fetchCategoriesFailure = (error) => {
-  createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error);
-};
 
-// export const fetchCategoriesStartAsync = async (dispatch) => {
-//   dispatch(fetchCategoriesStart());
-//   try {
-//     const categoriesArray = await getCategoriesAndDocuments("categories");
-//     console.log(categoriesArray);
-//     dispatch(fetchCategoriesSuccess(categoriesArray));
-//   } catch (error) {
-//     dispatch(fetchCategoriesFailure(error));
-//   }
-// };
+export const fetchCategoriesFailure = (error) =>
+  createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error);
 
 export const fetchCategoriesStartAsync = () => {
   return async (dispatch) => {
@@ -40,3 +25,14 @@ export const fetchCategoriesStartAsync = () => {
     }
   };
 };
+
+// export const fetchCategoriesStartAsync = async (dispatch) => {
+//   dispatch(fetchCategoriesStart());
+//   try {
+//     const categoriesArray = await getCategoriesAndDocuments("categories");
+//     console.log(categoriesArray);
+//     dispatch(fetchCategoriesSuccess(categoriesArray));
+//   } catch (error) {
+//     dispatch(fetchCategoriesFailure(error));
+//   }
+// };
