4f475ad9cdf0439da7d73a2f0023a530fb19b89d
DaDDy <blacklistuser420@gmail.com>
2023-05-29 18:06:34 +0630

    error with fetch url

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -22,12 +22,30 @@ const PaymentFrom = () => {
     const {
       paymentIntent: { client_secret },
     } = response;
+
     console.log(client_secret);
+
+    const paymentResult = await stripe.confirmCardPayment(client_secret, {
+      payment_method: {
+        card: elements.getElement(CardElement),
+        billing_details: {
+          name: "DaDDy",
+        },
+      },
+    });
+
+    if (paymentResult.error) {
+      alert(paymentResult.error.message);
+    } else {
+      if (paymentResult.paymentIntent.status === "succeeded") {
+        alert("Payment Successful");
+      }
+    }
   };
 
   return (
     <PaymentFromContainer>
-      <FormContainer>
+      <FormContainer onSubmit={paymentHander}>
         <h2>Credit Card Payment: </h2>
         <CardElement />
         <Button buttonType={BUTTON_TYPES_CLASSES.inverted}>Pay Now</Button>
